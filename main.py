f06503625e926115c0500269a76d5b7f4cc5ecda
Shahriar-Nibir <shahriarnibir615@gmail.com>
2021-02-04 13:46:38 +0600

    2nd commit

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pyqrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
 
-
+# Generate qrcode
 content = input("Give the text:")
 qrgennerate = pyqrcode.create(content)
 qrgennerate.png("content.png", scale=8)
+
+# scan the qrcode
+qr1 = decode(Image.open("content.png"))
+print(qr1[0].data.decode('ascii'))
